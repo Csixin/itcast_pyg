8c30f4b21882686df818cbb9363a31fe767c4142
Csixin <760186904@qq.com>
2019-03-09 17:22:42 +0800

    试一试

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,28 +1,32 @@
-$(function () {
+"use strict";
 
+$(function () {
   init();
 
   function init() {
     swiperDate();
   }
 
-  function swiperDate(){
+  function swiperDate() {
     $.ajax({
       type: "get",
       url: "http://api.pyg.ak48.xyz/api/public/v1/home/swiperdata",
       dataType: "json",
-      success: function (res) {
+      success: function success(res) {
         if (res.meta.status == 200) {
-          let data = res.data;
-          let html = template('swiperTpl',{arr:data});
+          var data = res.data;
+          var html = template('swiperTpl', {
+            arr: data
+          });
           $('.mui-slider').html(html);
           var gallery = mui('.mui-slider');
           gallery.slider({
             interval: 3000 //自动轮播周期，若为0则不自动播放，默认为0；
+
           });
         }
       }
     });
   }
-
-})
+});
+//# sourceMappingURL=index.js.map
